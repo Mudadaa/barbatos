5c4021e7aae45e42ea2592e6138a347251cd58cd
Mudadaa <layeb.hassan@gmail.com>
2023-09-10 03:49:33 +0200

    galerie in order&animation in progress

--- public/app.js
+++ public/app.js
@@ -5,14 +5,22 @@
 const app = {
   
   
-  // Sélectionner les éléments p de traduction
+ 
   init: function(){
+     // Sélection des éléments p de traduction
     app.english = document.querySelector(".english");
     app.japanese = document.querySelector(".japanese");
     app.english.addEventListener("click", app.getEnglish);
     app.japanese.addEventListener("click", app.getJapanese);
     app.englishP=document.getElementById("english");
     app.japaneseP=document.getElementById("japanese");
+//Sélection des img de galerie
+   app.upper=document.getElementById("upperBody");
+   app.lupus=document.getElementById("lupus");
+   app.weapon=document.getElementById("weapon");
+   //hover events
+   app.upper.addEventListener("mouseover",app.getLupus);
+   app.lupus.addEventListener("mouseover", app.getWeapon);
    },
   // Définir les chemins d'accès aux images
   
@@ -28,7 +36,14 @@ app.englishP.setAttribute("hidden","true");
   app.englishP.removeAttribute("hidden");
   app.japaneseP.setAttribute("hidden","true");
  },
-
+getLupus: function(){
+  console.log("hola los lupus de la venga")
+  app.lupus.removeAttribute("hidden")
+  
+},
+getWeapon: function(){
+  app.weapon.removeAttribute("hidden")
+}
 
 }
 document.addEventListener('DOMContentLoaded', app.init);
